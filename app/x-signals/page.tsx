d1949fd03482288fbfad11d5a43ed274b69f3d21
jackyecho1029 <528822833@qq.com>
2026-02-04 03:08:44 +0800

    fix: add ISR revalidate to x-signals page to show new posts

--- app/x-signals/page.tsx
+++ app/x-signals/page.tsx
@@ -2,6 +2,9 @@
 import { getSortedXSignalPostsData } from "../../lib/x-signals";
 import XSignalsClient from "./XSignalsClient";
 
+// Revalidate every 60 seconds to pick up new posts
+export const revalidate = 60;
+
 export default function XSignalsPage() {
     const posts = getSortedXSignalPostsData();
     return <XSignalsClient posts={posts} />;
